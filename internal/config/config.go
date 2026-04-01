bdb32d853a451724463c64e585df6d27b1d4a8a7
SOUMYA <sabatsoumya05@gmail.com>
2026-04-01 08:55:52 +0530

    parts_config_file

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,10 @@ package config
 
 import (
 	"flag"
+	"log"
 	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
@@ -16,7 +19,7 @@ type Config struct {
 	HTTPServer  `yaml:"http_server"` //embedding another struct
 }
 
-func MustLoad() {
+func MustLoad() *Config {
 	var configPath string
 	configPath = os.Getenv("CONFIG_PATH")
 	if configPath == ""{
@@ -24,5 +27,20 @@ func MustLoad() {
 		flags := flag.String("config","","path to the configuration file")
 		flag.Parse()
 		configPath = *flags
+		if configPath==""{
+			log.Fatal("Config path is not set")
+		}
 	}
+
+	//chk wheather that the file is availiable or not 
+	if _,err := os.Stat(configPath); os.IsNotExist(err){
+		log.Fatalf("config file does not exist: %s",configPath)
+	}
+
+	var cfg Config
+	err:=cleanenv.ReadConfig(configPath,&cfg)
+	if err!=nil{
+		log.Fatalf("cannot read the config file : %s",err.Error())
+	}
+	return &cfg 
 }
