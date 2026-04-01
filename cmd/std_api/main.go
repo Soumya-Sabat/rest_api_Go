9d071f1034494d41c57ba93108f767e3c86d0f3e
SOUMYA <sabatsoumya05@gmail.com>
2026-04-01 10:06:49 +0530

    first_instance_server

--- cmd/std_api/main.go
+++ cmd/std_api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ghost/restAPI/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students api")
+	//load config 
+	cfg := config.MustLoad()
+	//set up the logger through the package
+	//database setup
+	//setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /",func(w http.ResponseWriter,r *http.Request){
+		w.Write([]byte("Welcome to student API"))
+	})
+
+	//setup server
+	server:=http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+	fmt.Printf("server started at port : %s",cfg.HTTPServer.Addr)
+	err:=server.ListenAndServe()
+	if err!=nil{
+		log.Fatal("Failed to start the server")
+	}
 }
